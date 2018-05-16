1df6daf66d30b61bf2c6625bfd183fd4a2d264c4
Will Chou <willchou@willde-macbook-pro.local>
2018-05-16 15:48:16 +0800

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Will Chou on 2018/5/16.
+//  Copyright © 2018年 Will Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
