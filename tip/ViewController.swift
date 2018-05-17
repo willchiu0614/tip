f4402e653313b59050f9a39a0ef3193d55aed59e
Will Chou <willchou@willde-macbook-pro.local>
2018-05-17 21:02:39 +0800

    finish

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var totalcost=0.0
+    
+    
+   
+    @IBOutlet weak var itemcost: UITextView!
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var tipTextField: UITextField!
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBAction func addButton(_ sender: Any) {
+        view.endEditing(true)
+        if let moneyText=moneyTextField.text,let itemname=itemTextField.text,let tipText=tipTextField.text,let money=Double(moneyText),let tip=Double(tipText){
+            moneyTextField.text=""
+            itemTextField.text=""
+            tipTextField.text=""
+            let labeladd=itemcost.text
+           itemcost.text="\(labeladd!)"+"\n"+"\(itemname)"+" "+":\(money+tip*money/100)"
+        
+            totalcost+=money+tip*money/100
+        }
+    }
+    @IBAction func calculateButton(_ sender: Any) {
+            totalLabel.text="\(totalcost)"
+    }
+    @IBAction func tap(_ sender: Any) {
+        view.endEditing(true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
